85cd87ad1abfa4b69bff33cec29776f5fd3393b8
wujunyang <wujunyang@126.com>
2016-06-18 23:50:45 +0800

    增加项目

---
+++ WjyTestClassDemo/WjyTestClassDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WjyTestClassDemo
+//
+//  Created by wujunyang on 16/6/18.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WjyTestClassDemo/WjyTestClassDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WjyTestClassDemo
+//
+//  Created by wujunyang on 16/6/18.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
