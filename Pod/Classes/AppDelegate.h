8f6e562d6bd85d2eb565cf67d387b323a6a2f72e
wujunyang <wujunyang@126.com>
2016-06-20 22:44:43 +0800

    v0.0.4

---
+++ Pod/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHLog
+//
+//  Created by CatchZeng on 15/9/29.
+//  Copyright © 2015年 qingningxiezuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
